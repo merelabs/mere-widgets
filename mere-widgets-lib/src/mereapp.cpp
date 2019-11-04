1803c548fdbb4d7a0b8af610092084b9577b85c6
Bhuiyan Mohammad Iklash <bhuiyan.mohammad.iklash@gmail.com>
2019-11-05 03:12:56 +0600

    Allow to set app-code

--- mere-widgets-lib/src/mereapp.cpp
+++ mere-widgets-lib/src/mereapp.cpp
@@ -1,7 +1,24 @@
 #include "mereapp.h"
 
+MereApp::~MereApp()
+{
+
+}
+
 MereApp::MereApp(int &argc, char **argv)
     : QApplication(argc, argv)
 {
 
 }
+
+QString MereApp::code() const
+{
+    return m_code;
+}
+
+void MereApp::setCode(QString code)
+{
+    m_code = code;
+    emit codeChanged(m_code);
+}
+
--- mere-widgets-lib/src/mereapp.h
+++ mere-widgets-lib/src/mereapp.h
@@ -6,13 +6,19 @@
 class MereApp : public QApplication
 {
     Q_OBJECT
-//    Q_PROPERTY(QString appCode READ appCode WRITE setAppCode NOTIFY appCodeChanged)
 public:
+    virtual ~MereApp();
     explicit MereApp(int &argc, char **argv);
 
+    QString code() const;
+    void setCode(QString code);
+
 signals:
+    void codeChanged(const QString &code);
 
 public slots:
+private:
+    QString m_code;
 };
 
 #endif // MEREAPP_H
