c00eced836ae2f077c817cd187fe0679f1164e6f
Bhuiyan Mohammad Iklash <bmiklash@gmail.com>
2019-11-08 20:44:46 +0600

    remove commented unnecessary code

--- mere-widgets-lib/src/merewinheader.cpp
+++ mere-widgets-lib/src/merewinheader.cpp
@@ -16,8 +16,6 @@ MereWinHeader::MereWinHeader(QWidget *parent)
 
     QHBoxLayout *layout = new QHBoxLayout(this);
     layout->setContentsMargins(5, 5, 5, 5);
-
-//    initUI();
 }
 
 void MereWinHeader::initUI()
@@ -26,49 +24,6 @@ void MereWinHeader::initUI()
     initCenterPanel();
     initRightPanel();
 }
-
-//void MereWinHeader::initLeftPanel()
-//{
-//    QWidget *pane = new QWidget(this);
-//    pane->setMaximumWidth(100);
-//    layout()->addWidget(pane);
-//}
-
-//void MereWinHeader::initCenterPanel()
-//{
-//    QWidget *pane = new QWidget(this);
-//    layout()->addWidget(pane);
-
-//    QVBoxLayout *layout = new QVBoxLayout(pane);
-//    layout->setContentsMargins(0, 0, 0, 0);
-//    layout->setSpacing(3);
-//    layout->setAlignment(Qt::AlignCenter | Qt::AlignHCenter);
-
-//     m_title = new QLabel("[Unknown app]");
-//     m_title->setAlignment(Qt::AlignCenter | Qt::AlignHCenter);
-//     m_title->setObjectName("MereWinHeaderTitle");
-//     layout->addWidget(m_title);
-
-//     QLabel *host = new QLabel(QString("@").append(QHostInfo::localHostName()));
-//     host->setAlignment(Qt::AlignCenter | Qt::AlignHCenter);
-//     layout->addWidget(host);
-//}
-
-//void MereWinHeader::initRightPanel()
-//{
-//    QWidget *pane = new QWidget(this);
-//    pane->setMaximumWidth(100);
-//    layout()->addWidget(pane);
-
-//    QHBoxLayout *layout = new QHBoxLayout(pane);
-//    layout->setAlignment(Qt::AlignRight);
-
-//    QPushButton *close = new QPushButton(QIcon(":/pkg/close.png"),"");
-//    close->setMaximumWidth(24);
-//    close->setFlat(true);
-//    layout->addWidget(close);
-//    connect(close, SIGNAL(clicked(bool)), this, SLOT(close()));
-//}
 
 void MereWinHeader::setTitle(const QString title)
 {
